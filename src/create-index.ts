9e47154934d45fd1233ba77b6f821a4ae901bf35
Uday Maroju <udaymaroju9@gmail.com>
2024-09-16 12:00:21 +0530

    first update

--- src/create-index.ts
+++ src/create-index.ts
@@ -6,7 +6,7 @@ async function addIndex() {
     const createIndexQuery = 'CREATE INDEX idx_todos_user_id ON todos(user_id)';
     await client.query(createIndexQuery);
     
-    console.log("Index added successfully on user_id column of todos table!");
+    console.log("Index added successfully on user_id column of todos table.");
 }
 
 addIndex();
